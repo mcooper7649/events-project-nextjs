c461853f93ccdf7d406a671b9e03cbb09d4fea08
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-11-09 21:32:29 -0500

    added event detail content with Dynamic routing

--- pages/events/[eventId].js
+++ pages/events/[eventId].js
@@ -1,8 +1,32 @@
+import {useRouter} from 'next/router'
+import { getEventById } from '../../dummy-data';
+import EventSummary from '../../components/event-detail/event-summary'
+import EventLogistics from '../../components/event-detail/event-logistics'
+import EventContent from '../../components/event-detail/event-content';
+
+import { Fragment } from 'react';
+
 function EventDetail (){
+
+    const router = useRouter();
+
+    const eventId = router.query.eventId
+    const event = getEventById(eventId);
+
+    if (!event){
+        return <p>No Event found!</p>
+    }
+
     return (
-        <div>
-            <h1>Welcome to the EventDetail HomePage</h1>
-        </div>
+       <Fragment>
+           <EventSummary title={event.title}/>
+           <EventLogistics date={event.date} address={event.location} image={event.image} imageAlt={event.title}/>
+           <EventContent>
+               <p>
+                   {event.description}
+               </p>
+           </EventContent>
+       </Fragment>
     )
 }
 
