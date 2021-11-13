e71d8fc771f15f7d8f1b8c41eb276474df697120
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-11-13 17:23:58 -0500

    done

--- pages/events/[...slug].js
+++ pages/events/[...slug].js
@@ -1,14 +1,48 @@
-import {useRouter} from 'next/router';
+import { useRouter } from "next/router";
+import { getFilteredEvents } from '../../dummy-data'
+import EventList from "../../components/events/event-list";
 
+function FilteredEventsPage() {
+  const router = useRouter();
 
+  const filterData = router.query.slug;
 
+  console.log(filterData);
 
-function FilteredEvent(){
-    return (
-        <div>
-            <h1>Welcome to the FilteredEvent HomePage</h1>
-        </div>
-    )
+  if (!filterData) {
+    return <p className="center">loading...</p>;
+  }
+
+  const filteredYear = filterData[0];
+  const filteredMonth = filterData[1];
+
+  const numYear = +filteredYear;
+  const numMonth = +filteredMonth;
+
+  if (
+    isNaN(numMonth) ||
+    isNaN(numMonth) ||
+    numYear > 2030 ||
+    numYear < 2021 ||
+    numMonth < 1 ||
+    numMonth > 12
+  ) {
+    return <p>Invalid filter. Please adjust your values</p>;
+  }
+
+  const filteredEvents = getFilteredEvents({
+    year: numYear,
+    month: numMonth,
+  });
+
+  if (!filteredEvents || filteredEvents.length === 0) {
+    return <p>No events founds for the chosen filter!</p>;
+  }
+
+  return (
+    <div>
+      <EventList items={filteredEvents} />
+    </div>
+  );
 }
-
-export default FilteredEvent;
+export default FilteredEventsPage;
