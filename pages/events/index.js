2319074e9b94e804f9a2d8cd2846ebd293f6b0be
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2021-11-18 23:13:53 -0500

    update all Event Page and removed Dummy Data

--- pages/events/index.js
+++ pages/events/index.js
@@ -1,26 +1,34 @@
-
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../helpers/api-util";
 import EventList from "../../components/events/event-list";
-import EventsSearch from '../../components/events/events-search';
+import EventsSearch from "../../components/events/events-search";
 import { Fragment } from "react";
-import {useRouter} from 'next/router'
+import { useRouter } from "next/router";
 
-function AllEventsPage (){
-    const router = useRouter();
-    const events = getAllEvents()
+function AllEventsPage(props) {
+  const { events } = props;
 
-    function findEventsHandler(year, month){
-    const fullPath = `/events/${year}/${month}`        
-        
-        router.push(fullPath);
-    }
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
 
-    return (
-        <Fragment>
-        <EventsSearch onSearch={findEventsHandler}/>
-        <EventList items={events} />
-        </Fragment>
-    )
+    router.push(fullPath);
+  }
+
+  return (
+    <Fragment>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </Fragment>
+  );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+  return {
+    props: {
+      events: events,
+    },
+    revalidate: 60
+  };
 }
 
 export default AllEventsPage;
