2e505dbb00e49456e3ed33e458dc77c435169de5
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-03-21 10:03:15 -0400

    added notification context for registration

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,12 +1,23 @@
-import { Fragment } from 'react';
-
+import { Fragment, useContext } from 'react';
+import Notification from '../ui/notification';
+import NotificationContext from '../../store/notification-context';
 import MainHeader from './main-header';
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+
+  const activeNotifiication = notificationCtx.notification;
   return (
     <Fragment>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotifiication && (
+        <Notification
+          title={activeNotifiication.title}
+          message={activeNotifiication.message}
+          status={activeNotifiication.status}
+        />
+      )}
     </Fragment>
   );
 }
