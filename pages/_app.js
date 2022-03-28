2e6bc92df5352f0d69f64b1c0f5160a7e7b59d20
mcooper7649 <55610025+mcooper7649@users.noreply.github.com>
2022-03-28 12:35:04 -0400

    update meta data

--- pages/_app.js
+++ pages/_app.js
@@ -11,11 +11,21 @@ function MyApp({ Component, pageProps }) {
         <Head>
           <title>Next Events</title>
           <meta
+            name="publish_date"
+            property="og:publish_date"
+            content="2022-03-21T00:00:00-0600"
+          />
+          <meta
             name="title"
             content="Networking and Events Finder | NextJS"
             property="og:title"
           />
-          <meta name="description" content="Find Events Near You..." />
+          <meta
+            name="description"
+            content="Find local, remote and digital events today. All built using the NextJS framework. 
+            Meet like minded people without having to wait for horrific page loading speeds and terrible seo for your event. NextJS offers React 
+            functionality with the added bonuses of Server-Side Rendering"
+          />
           <meta
             name="viewport"
             content="initial-scale=1.0, width=device-width"
